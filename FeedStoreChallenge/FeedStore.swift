0c082834ac0599c4c047c2257172d9d766868367
Пильтенко Роман Сергеевич <roman.piltenko@goods.ru>
2021-02-13 23:11:04 +0300

    Add actual implementation to `RealmStore`

--- FeedStoreChallenge/FeedStore.swift
+++ FeedStoreChallenge/FeedStore.swift
@@ -29,20 +29,89 @@ public protocol FeedStore {
 	func retrieve(completion: @escaping RetrievalCompletion)
 }
 
+final class ImageRealmObject: Object {
+	@objc dynamic var imageIdString: String = ""
+	@objc dynamic var imageDescription: String?
+	@objc dynamic var imageLocation: String?
+	@objc dynamic var imageUrlString: String = ""
+	let feed = LinkingObjects(fromType: FeedRealmObject.self, property: "images")
+}
+
+final class FeedRealmObject: Object {
+	@objc dynamic var objID: ObjectId = ObjectId()
+	@objc dynamic var timestamp: Date = Date.init()
+	let images = List<ImageRealmObject>()
+	
+	override class func primaryKey() -> String? {
+		"objID"
+	}
+}
+
 public final class RealmStore: FeedStore {
-	public init() {
-		
+	
+	private let configuration: Realm.Configuration
+	
+	public init(fileURL: URL) {
+		self.configuration = Realm.Configuration(fileURL: fileURL, schemaVersion: 1, migrationBlock: { _, oldSchemaVersion in
+			if (oldSchemaVersion < 1) {
+			  // Nothing to do!
+			  // Realm will automatically detect new properties and removed properties
+			  // And will update the schema on disk automatically
+			}
+		})
 	}
 	
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-		
+		do {
+			let database = try Realm(configuration: configuration)
+			try database.write {
+				database.deleteAll()
+				completion(nil)
+			}
+		} catch let error as NSError {
+			completion(error)
+		}
 	}
 	
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		
+		do {
+			let database = try Realm(configuration: configuration)
+			let realmImages: [ImageRealmObject] = feed.map { ImageRealmObject(value: ["imageIdString": $0.id.uuidString,
+																					  "imageDescription": $0.description,
+																					  "imageLocation": $0.location,
+																					  "imageUrlString": $0.url.absoluteString]) }
+			try database.write {
+				let realmFeed = FeedRealmObject(value: ["timestamp": timestamp,
+														"images": realmImages])
+				database.add(realmFeed, update: .all)
+				completion(nil)
+			}
+			
+		} catch let error as NSError {
+			completion(error)
+		}
 	}
 	
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		do {
+			let database = try Realm(configuration: configuration)
+			let result = database.objects(FeedRealmObject.self)
+			if let firstResult = result.first {
+				let timestamp = firstResult.timestamp
+				let localImages: [LocalFeedImage] = firstResult.images.map { LocalFeedImage(id: UUID(uuidString: $0.imageIdString)!,
+																							description: $0.imageDescription,
+																							location: $0.imageLocation,
+																							url: URL(string: $0.imageUrlString)!) }
+				if localImages.isEmpty {
+					completion(.empty)
+				} else {
+					completion(.found(feed: localImages, timestamp: timestamp))
+				}
+			} else {
+				completion(.empty)
+			}
+		} catch let error as NSError {
+			completion(.failure(error))
+		}
 	}
 }
